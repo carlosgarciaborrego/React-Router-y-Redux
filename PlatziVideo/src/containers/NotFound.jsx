524c12c84c5f408aec114c107c0062541a20c211
Carlos Jesus Garcia Borrego <carlosjesusgarciaborrego@gmail.com>
2020-05-02 20:19:36 +0200

    Primera parte del curso

---
+++ PlatziVideo/src/containers/NotFound.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFound = () => (
+    //Se podria quitar el React.Fragment y hace la misma función.(esa es la forma breve)
+    <React.Fragment>    
+        <h1>No encontrado</h1>
+    </React.Fragment>
+);
+
+export default NotFound;
